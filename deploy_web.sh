6298978ab5782df57564609e3b15a994fd1559e1
AisukoWasTaken <zdjeblou@etud.u-pem.fr>
2016-02-29 14:12:26 +0100

    Added route access for SMS reading EventBus

--- deploy_web.sh
+++ deploy_web.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$(id -u)" != "0" ]; then
-	echo "Sorry, you are not root."
-	exit 1
+    echo "Sorry, you are not root."
+    exit 1
 fi
 wwwfolder=/var/www/badimsiweb
 sudo mkdir -p $wwwfolder/*
